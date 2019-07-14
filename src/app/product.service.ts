fca692c7d2c0a94db5642e13ff4b40ad8f6468b1
allmi <allmightypol@gmail.com>
2019-07-14 14:18:48 +0200

    adding product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,18 +2,20 @@ import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 
 import 'rxjs/add/operator/map';
+import {map} from "rxjs/operators";
 
 @Injectable()
 export class ProductService {
 
-  private albumUrl = '..assets/album.json';
+  private albumUrl = '../assets/album.json';
 
   constructor(private http: Http) {
   }
 
   getAlbum(id: number) {
     return this.http.get(this.albumUrl)
-      .map((response) => response.json())
+      .pipe(
+        map((response) => response.json())
+      );
   }
-
 }
